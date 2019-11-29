21eb5377990d525ae182b46ae139a9f9df47c820
Elisha <elishadar@gmail.com>
2019-11-29 15:22:59 +0200

    added graphics for the main activity, started functionality

--- app/src/main/java/edu/ariel/SE_project/worki/MainActivity.java
+++ app/src/main/java/edu/ariel/SE_project/worki/MainActivity.java
@@ -1,13 +1,25 @@
 package edu.ariel.SE_project.worki;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import edu.ariel.SE_project.worki.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    Button login;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(Helper.ClickTransfer(this, new Intent(this, LoginActivity.class)));
     }
 }
