f97938d26b12c0de9c2e8206d69a5cb629e3842a
Amit <amitkazado@gmail.com>
2019-12-19 18:53:27 +0200

    added new toolbar menu

--- app/src/main/java/edu/ariel/SE_project/worki/MainActivity.java
+++ app/src/main/java/edu/ariel/SE_project/worki/MainActivity.java
@@ -1,11 +1,16 @@
 package edu.ariel.SE_project.worki;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button login;
     private Button register;
     private FirebaseAuth mAuth;
+//    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -38,6 +44,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         login.setOnClickListener(this);
         register.setOnClickListener(this);
+
+
     }
 
     private void updateUI(FirebaseUser currentUser)
@@ -61,4 +69,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         startActivity(intent);
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.toolbar_menu, menu);
+//        return true;
+//    }
+
+    // This method allows the user to navigate on the app through the menu
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//
+//        Intent intent;
+//        switch (item.getItemId()){
+//            case R.id.toolbarHomePage:{
+//                intent = new Intent(MainActivity.this, TimerActivity.class);
+//                startActivity(intent);
+//                return true;
+//            }
+//            case R.id.toolbarAskForShifts:{
+//                intent = new Intent(MainActivity.this, ShiftsActivity.class);
+//                startActivity(intent);
+//                return true;
+//            }
+//            case R.id.toolbarWhosOnline:{
+//                intent = new Intent(MainActivity.this, OnlineActivity.class);
+//                startActivity(intent);
+//                return true;
+//            }
+//            case R.id.toolbarSettings:{
+//                intent = new Intent(MainActivity.this, SettingsActivity.class);
+//                startActivity(intent);
+//                return true;
+//            }
+//            case R.id.toolbarLogout:{
+//                intent = new Intent(MainActivity.this, MainActivity.class);
+//                startActivity(intent);
+//                return true;
+//            }
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
