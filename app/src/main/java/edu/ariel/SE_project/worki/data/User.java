8f65768bb2ec9a9dea9f6eba47c357289aa2da25
Elisha <elishadar@gmail.com>
2019-12-23 13:26:17 +0200

    added 'CurrentUser' to get current user data

--- app/src/main/java/edu/ariel/SE_project/worki/data/User.java
+++ app/src/main/java/edu/ariel/SE_project/worki/data/User.java
@@ -1,6 +1,7 @@
 package edu.ariel.SE_project.worki.data;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 
 /**
  * An object to store user data.
@@ -9,6 +10,7 @@ public class User
 {
     public int id;
     public String name;
+    public boolean isManager;
 
     /**
      * Create an empty user object.
@@ -31,13 +33,19 @@ public class User
     }
 
     /**
-     * Read user data from Firebase.
+     * Create a user using a data snapshot of the storage.
      *
-     * @param user the firebase user.
+     * @param snapshot the DataSnapshot of the storage.
      */
-    public User(FirebaseUser user)
+    User(FirebaseUser user, DataSnapshot snapshot)
     {
+
         id = Integer.parseInt(user.getUid());
-        name = user.getDisplayName();
+
+        name = snapshot.child("name").getValue(String.class);
+        Boolean manager = snapshot.child("manager").getValue(boolean.class);
+        if (manager != null)
+            isManager = manager;
+
     }
 }
