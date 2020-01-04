886fc590dee9a305cb3b2635a44c2c575b1752a7
Amit <amitkazado@gmail.com>
2020-01-04 18:18:39 +0200

    added basic shift class

---
+++ app/src/main/java/edu/ariel/SE_project/worki/data/Shift.java
@@ -0,0 +1,81 @@
+package edu.ariel.SE_project.worki.data;
+
+import androidx.annotation.RequiresPermission;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Shift implements ReadableFromDatabase,WriteableToDatabase
+{
+    private static int shiftIdGenerator = 0;
+    private User shiftManager;
+    private ArrayList<User> workersInShift;
+    private int shiftId;
+    private Date shiftDate;
+    private Date shiftEnd;
+
+    public Shift()
+    {
+
+    }
+
+    public Shift (User shiftManager, int shiftLength)
+    {
+        this.shiftManager = shiftManager;
+        this.shiftId = shiftIdGenerator++;
+        shiftDate = new Date();
+        shiftEnd = new Date(shiftDate.getTime() + shiftLength);
+
+    }
+
+    public Shift (Shift shift)
+    {
+        this.shiftManager = shift.shiftManager;
+        this.shiftId = shift.shiftId;
+        this.workersInShift = shift.workersInShift;
+        this.shiftDate = shift.shiftDate;
+        this.shiftEnd = shift.shiftEnd;
+    }
+
+    public void addWorkerToShift(User worker)
+    {
+        workersInShift.add(worker);
+    }
+
+    public void removeWorkerFromShift(User worker)
+    {
+        workersInShift.remove(worker);
+    }
+
+    public String toString()
+    {
+        String workers = workersInShift.toString();
+
+        return "Shift {Shift Id: " + shiftId + '\'' +
+                ", Manager Id: " + shiftManager.id + '\'' +
+                ", Worker on Shift" + workers + '\'' +
+                ", Shift Starts in: " + shiftDate.toString() +
+                ", Shift Ends in: " + shiftEnd.toString() + '\'' +
+                '}';
+    }
+
+    /**
+     * Read a object from the database.
+     *
+     * @param snapshot the database snapshot where the object is stored.
+     */
+    @Override
+    public ReadableFromDatabase readFromDatabase(DataSnapshot snapshot)
+    {
+        return new Shift();
+    }
+
+    @Override
+    public void writeToDatabase(DatabaseReference reference)
+    {
+
+    }
+}
