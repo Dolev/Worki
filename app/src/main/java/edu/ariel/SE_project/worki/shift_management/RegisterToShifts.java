214afc322170cad0e793085145f1d4b65f6adc07
Elisha <elishadar@gmail.com>
2020-01-07 19:38:47 +0200

    minor changes

---
+++ app/src/main/java/edu/ariel/SE_project/worki/shift_management/RegisterToShifts.java
@@ -0,0 +1,18 @@
+package edu.ariel.SE_project.worki.shift_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.ariel.SE_project.worki.R;
+
+public class RegisterToShifts extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_to_shifts);
+    }
+}
